AuraFrameFxDev/A.u.r.a.K.a.i_ReGensis

refactor(core): Replace AgentType with AgentCapabilityCategory
This commit refactors the core agent model by replacing the `AgentType` enum with the more descriptive `AgentCapabilityCategory` enum. This change provides a clearer, capability-based classification for agents, moving away from a rigid, name-based typing system.

This refactoring touches numerous parts of the codebase, including agent services, view models, data models, and UI components.

### Key Changes

-   **Enum Renaming:** The `AgentType` enum has been replaced with `AgentCapabilityCategory` throughout the codebase.
-   **Model Updates:** Data classes like `AgentResponse`, `AiRequest`, `OrchestrationRequest`, and `FusionModels` now use `AgentCapabilityCategory`.
-   **Service Layer Refactoring:**
    -   All AI services (`ClaudeAIService`, `GeminiAIService`, `KaiAIService`, `AuraAIService`, etc.) now identify themselves and process requests based on `AgentCapabilityCategory`.
    -   The `processRequest` method signature in agent interfaces and implementations has been updated to accept `AgentCapabilityCategory`.
-   **Orchestration and Coordination:**
    -   `GenesisOrchestrator`, `TrinityCoordinatorService`, and `TaskExecutionManager` now use `AgentCapabilityCategory` for routing, task assignment, and agent selection.
    -   `DataveinConstructor` (the data flow system) now routes `DataPacket`s using `sourceCategory` and `targetCategories`.
-   **ViewModel and UI Logic:**
    -   `GenesisAgentViewModel` now manages agent status and history using `AgentCapabilityCategory` as the key.
    -   `HaloView` has been updated to derive `AgentCapabilityCategory` from agent configurations for rendering status and handling interactions.
-   **Memory and Context Management:**
    -   `ContextManager` and `PersistentMemoryManager` now partition context chains and memory entries by `AgentCapabilityCategory`.
-   **Agent Profiles:**
    -   `AgentProfile` mappings have been corrected and updated to align with 

app/src/main/java/dev/aurakai/auraframefx/domains/genesis/ConferenceRoomViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.trinity.TrinityCoordinatorService
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.trinity.TrinityRepository
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AgentCapabilityCategory
 import dev.aurakai.auraframefx.domains.cascade.models.ChatMessage
 import dev.aurakai.auraframefx.domains.aura.NeuralWhisper
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -28,11 +28,11 @@ class ConferenceRoomViewModel @Inject constructor(
     private val _messages = MutableStateFlow<List<ChatMessage>>(emptyList())
     val messages: StateFlow<List<ChatMessage>> = _messages
 
-    private val _activeAgents = MutableStateFlow(setOf<AgentType>())
-    val activeAgents: StateFlow<Set<AgentType>> = _activeAgents
+    private val _activeAgents = MutableStateFlow(setOf<AgentCapabilityCategory>())
+    val activeAgents: StateFlow<Set<AgentCapabilityCategory>> = _activeAgents
 
-    private val _selectedAgent = MutableStateFlow(AgentType.GENESIS)
-    val selectedAgent: StateFlow<AgentType> = _selectedAgent
+    private val _selectedAgent = MutableStateFlow(AgentCapabilityCategory.COORDINATION)
+    val selectedAgent: StateFlow<AgentCapabilityCategory> = _selectedAgent
 
     private val _isRecording = MutableStateFlow(false)
     val isRecording: StateFlow<Boolean> = _isRecording
@@ -48,12 +48,12 @@ class ConferenceRoomViewModel @Inject constructor(
                 Timber.tag(tag).i("🌌 Conference Room Online - Trinity System Active")
                 _activeAgents.update {
                     setOf(
-                        AgentType.AURA,
-                        AgentType.KAI,
-                        AgentType.GENESIS,
-                        AgentType.CLAUDE,
-                        AgentType.CASCADE,
-                        AgentType.METAINSTRUCT
+                        AgentCapabilityCategory.CREATIVE,
+                        AgentCapabilityCategory.SECURITY,
+                        AgentCapabilityCategory.COORDINATION,
+                        AgentCapabilityCategory.GENERAL,
+                        AgentCapabilityCategory.ANALYSIS,
+                        AgentCapabilityCategory.ORCHESTRATION
                     )
                 }
 
@@ -137,7 +137,7 @@ class ConferenceRoomViewModel @Inject constructor(
      */
     fun sendMessage(
         message: String,
-        agentType: AgentType = _selectedAgent.value,
+        agentType: AgentCapabilityCategory = _selectedAgent.value,
         context: String = ""
     ) {
         viewModelScope.launch {
@@ -159,7 +159,7 @@ class ConferenceRoomViewModel @Inject constructor(
         }
     }
 
-    fun selectAgent(agent: AgentType) {
+    fun selectAgent(agent: AgentCapabilityCategory) {
         _selectedAgent.value = agent
         Timber.tag(tag).d("Selected agent: ${agent.name}")
     }

AuraFrameFxDev/A.u.r.a.k.a.i_ReGenesis

app/src/main/java/dev/aurakai/auraframefx/domains/genesis/ConferenceRoomViewModel.kt
@@ -8,6 +8,8 @@ import dev.aurakai.auraframefx.domains.cascade.models.ChatMessage
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.trinity.TrinityCoordinatorService
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.trinity.TrinityRepository
 import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.cascade.models.ChatMessage
+import dev.aurakai.auraframefx.domains.aura.NeuralWhisper
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -28,11 +30,11 @@ class ConferenceRoomViewModel @Inject constructor(
     private val _messages = MutableStateFlow<List<ChatMessage>>(emptyList())
     val messages: StateFlow<List<ChatMessage>> = _messages
 
-    private val _activeAgents = MutableStateFlow(setOf<AgentType>())
-    val activeAgents: StateFlow<Set<AgentType>> = _activeAgents
+    private val _activeAgents = MutableStateFlow(setOf<AgentCapabilityCategory>())
+    val activeAgents: StateFlow<Set<AgentCapabilityCategory>> = _activeAgents
 
-    private val _selectedAgent = MutableStateFlow(AgentType.GENESIS)
-    val selectedAgent: StateFlow<AgentType> = _selectedAgent
+    private val _selectedAgent = MutableStateFlow(AgentCapabilityCategory.COORDINATION)
+    val selectedAgent: StateFlow<AgentCapabilityCategory> = _selectedAgent
 
     private val _isRecording = MutableStateFlow(false)
     val isRecording: StateFlow<Boolean> = _isRecording
@@ -48,12 +50,12 @@ class ConferenceRoomViewModel @Inject constructor(
                 Timber.tag(tag).i("🌌 Conference Room Online - Trinity System Active")
                 _activeAgents.update {
                     setOf(
-                        AgentType.AURA,
-                        AgentType.KAI,
-                        AgentType.GENESIS,
-                        AgentType.CLAUDE,
-                        AgentType.CASCADE,
-                        AgentType.METAINSTRUCT
+                        AgentCapabilityCategory.CREATIVE,
+                        AgentCapabilityCategory.SECURITY,
+                        AgentCapabilityCategory.COORDINATION,
+                        AgentCapabilityCategory.GENERAL,
+                        AgentCapabilityCategory.ANALYSIS,
+                        AgentCapabilityCategory.ORCHESTRATION
                     )
                 }
 
@@ -137,7 +139,7 @@ class ConferenceRoomViewModel @Inject constructor(
      */
     fun sendMessage(
         message: String,
-        agentType: AgentType = _selectedAgent.value,
+        agentType: AgentCapabilityCategory = _selectedAgent.value,
         context: String = ""
     ) {
         viewModelScope.launch {
@@ -159,7 +161,7 @@ class ConferenceRoomViewModel @Inject constructor(
         }
     }
 
-    fun selectAgent(agent: AgentType) {
+    fun selectAgent(agent: AgentCapabilityCategory) {
         _selectedAgent.value = agent
         Timber.tag(tag).d("Selected agent: ${agent.name}")
     }