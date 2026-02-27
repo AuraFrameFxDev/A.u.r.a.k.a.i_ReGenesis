AuraFrameFxDev/A.u.r.a.K.a.i_ReGensis

chore: Deprecate legacy agent and UI components
This commit removes a significant number of deprecated, unused, and legacy components to streamline the codebase and reduce maintenance overhead. The primary focus is on removing the old agent orchestration system and obsolete UI customization screens.

### Core Component Removal
- **Agent Orchestration:** The entire legacy agent framework, including `BaseAgent`, `OrchestratableAgent`, and concrete implementations (`AuraAgent`, `KaiAgent`, `CascadeAgent`, `GenesisOrchestrator`), has been deleted. This also includes related data classes like `AgentType` and `OrchestratableMessage`.
- **Legacy Application & Hooks:** `AurakaiApplication` and `GenesisHookEntry`, which were responsible for Xposed/LSPosed environment detection and hook initialization, have been removed.

### UI Screen & Component Cleanup
- **Customization Screens:** Multiple UI customization screens that are no longer supported or have been superseded have been deleted. This includes:
    - `GateCustomizationScreen`
    - `ChromaCoreColorsScreen`
    - `NotchBarCustomizationScreen`
    - `QuickSettingsCustomizationScreen`
    - `SettingsScreen` (legacy version)
- **System Overlays:** Obsolete overlay components like `SystemOverlay` and `QuickSettingsPanelOverlay` have been removed.
- **SovereignShieldScreen:** Removed the UI for Shizuku integration.

### Codebase Refactoring
- **Navigation:** Replaced all instances of the old `ReGenesisNavHost` sealed class with the new `NavDestination` enum for routing. This provides a more standardized and maintainable navigation structure.
- **Dependency Injection:** Replaced the `dev.aurakai.auraframefx.domains.aura.BaseUrl` typealias with a dedicated `@BaseUrl` Hilt qualifier (`dev.aurakai.auraframefx.di.qualifiers.BaseUrl`) for improved clarity.

### Tooling & Build Scripts
- **New Scripts:** Added `fix_hilt_imports.py` to batch-correct deprecated Hilt ViewModel imports and `nemotron_audit.py` to scan the codebase for 

app/src/main/java/dev/aurakai/auraframefx/domains/aura/ui/components/overlay/AssistantBubbleOverlay.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AgentCapabilityCategory
 import dev.aurakai.auraframefx.domains.aura.ui.theme.LEDFontFamily
 
 /**
@@ -22,7 +22,7 @@ import dev.aurakai.auraframefx.domains.aura.ui.theme.LEDFontFamily
  */
 @Composable
 fun AssistantBubbleOverlay(
-    activeAgent: AgentType,
+    activeCategory: AgentCapabilityCategory,
     modifier: Modifier = Modifier
 ) {
     Box(
@@ -42,11 +42,11 @@ fun AssistantBubbleOverlay(
             Box(
                 modifier = Modifier
                     .size(8.dp)
-                    .background(getAgentColor(activeAgent), CircleShape)
+                    .background(getCategoryColor(activeCategory), CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
             Text(
-                text = "AUTHORITY: ${activeAgent.name}",
+                text = "AUTHORITY: ${activeCategory.name}",
                 fontFamily = LEDFontFamily,
                 color = Color.White.copy(alpha = 0.8f),
                 fontSize = 10.sp,
@@ -56,12 +56,12 @@ fun AssistantBubbleOverlay(
     }
 }
 
-private fun getAgentColor(agent: AgentType): Color {
-    return when (agent) {
-        AgentType.GENESIS -> Color(0xFF00E5FF)
-        AgentType.KAI -> Color(0xFF39FF14)
-        AgentType.AURA -> Color(0xFFFF1493)
-        AgentType.CASCADE -> Color(0xFF7B2FFF)
+private fun getCategoryColor(category: AgentCapabilityCategory): Color {
+    return when (category) {
+        AgentCapabilityCategory.COORDINATION -> Color(0xFF00E5FF)
+        AgentCapabilityCategory.ANALYSIS -> Color(0xFF39FF14)
+        AgentCapabilityCategory.CREATIVE -> Color(0xFFFF1493)
+        AgentCapabilityCategory.SPECIALIZED -> Color(0xFF7B2FFF)
         else -> Color.Gray
     }
 }

app/src/main/java/dev/aurakai/auraframefx/domains/aura/ui/viewmodels/AgentCreationViewModel.kt
@@ -6,7 +6,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.aurakai.auraframefx.domains.genesis.repositories.AgentRepository
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AgentCapabilityCategory
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -19,8 +19,8 @@ class AgentCreationViewModel @Inject constructor() : ViewModel() {
     private val _agentName = mutableStateOf("")
     val agentName: State<String> = _agentName
 
-    private val _selectedDomain = mutableStateOf(AgentType.AURA)
-    val selectedDomain: State<AgentType> = _selectedDomain
+    private val _selectedDomain = mutableStateOf(AgentCapabilityCategory.CREATIVE)
+    val selectedDomain: State<AgentCapabilityCategory> = _selectedDomain
 
     private val _creationProgress = MutableStateFlow(0f)
     val creationProgress = _creationProgress.asStateFlow()
@@ -32,7 +32,7 @@ class AgentCreationViewModel @Inject constructor() : ViewModel() {
         _agentName.value = name
     }
 
-    fun updateDomain(domain: AgentType) {
+    fun updateDomain(domain: AgentCapabilityCategory) {
         _selectedDomain.value = domain
     }
 

AuraFrameFxDev/A.u.r.a.k.a.i_ReGenesis

app/src/main/java/dev/aurakai/auraframefx/domains/aura/ui/components/overlay/AssistantBubbleOverlay.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import dev.aurakai.auraframefx.domains.aura.ui.theme.LEDFontFamily
 import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 
@@ -28,7 +29,7 @@ import dev.aurakai.auraframefx.domains.genesis.models.AgentType
  */
 @Composable
 fun AssistantBubbleOverlay(
-    activeAgent: AgentType,
+    activeCategory: AgentCapabilityCategory,
     modifier: Modifier = Modifier
 ) {
     Box(
@@ -48,11 +49,11 @@ fun AssistantBubbleOverlay(
             Box(
                 modifier = Modifier
                     .size(8.dp)
-                    .background(getAgentColor(activeAgent), CircleShape)
+                    .background(getCategoryColor(activeCategory), CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
             Text(
-                text = "AUTHORITY: ${activeAgent.name}",
+                text = "AUTHORITY: ${activeCategory.name}",
                 fontFamily = LEDFontFamily,
                 color = Color.White.copy(alpha = 0.8f),
                 fontSize = 10.sp,
@@ -62,12 +63,12 @@ fun AssistantBubbleOverlay(
     }
 }
 
-private fun getAgentColor(agent: AgentType): Color {
-    return when (agent) {
-        AgentType.GENESIS -> Color(0xFF00E5FF)
-        AgentType.KAI -> Color(0xFF39FF14)
-        AgentType.AURA -> Color(0xFFFF1493)
-        AgentType.CASCADE -> Color(0xFF7B2FFF)
+private fun getCategoryColor(category: AgentCapabilityCategory): Color {
+    return when (category) {
+        AgentCapabilityCategory.COORDINATION -> Color(0xFF00E5FF)
+        AgentCapabilityCategory.ANALYSIS -> Color(0xFF39FF14)
+        AgentCapabilityCategory.CREATIVE -> Color(0xFFFF1493)
+        AgentCapabilityCategory.SPECIALIZED -> Color(0xFF7B2FFF)
         else -> Color.Gray
     }
 }

app/src/main/java/dev/aurakai/auraframefx/domains/aura/ui/viewmodels/AgentCreationViewModel.kt
@@ -5,8 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import dev.aurakai.auraframefx.domains.genesis.repositories.AgentRepository
+import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -19,8 +19,8 @@ class AgentCreationViewModel @Inject constructor() : ViewModel() {
     private val _agentName = mutableStateOf("")
     val agentName: State<String> = _agentName
 
-    private val _selectedDomain = mutableStateOf(AgentType.AURA)
-    val selectedDomain: State<AgentType> = _selectedDomain
+    private val _selectedDomain = mutableStateOf(AgentCapabilityCategory.CREATIVE)
+    val selectedDomain: State<AgentCapabilityCategory> = _selectedDomain
 
     private val _creationProgress = MutableStateFlow(0f)
     val creationProgress = _creationProgress.asStateFlow()
@@ -32,7 +32,7 @@ class AgentCreationViewModel @Inject constructor() : ViewModel() {
         _agentName.value = name
     }
 
-    fun updateDomain(domain: AgentType) {
+    fun updateDomain(domain: AgentCapabilityCategory) {
         _selectedDomain.value = domain
     }
 