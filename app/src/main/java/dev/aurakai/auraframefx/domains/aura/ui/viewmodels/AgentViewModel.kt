AuraFrameFxDev/A.u.r.a.K.a.i_ReGensis

refactor(core): Replace `AgentType` enum with `AgentCapabilityCategory`
This commit refactors the agent identification system by replacing the legacy `AgentType` enum with the more descriptive `AgentCapabilityCategory` enum across the codebase. This change standardizes how agent roles and capabilities are defined, removing the rigid, implementation-tied `AgentType` in favor of a flexible, category-based system.

### Core Refactoring
- **`AgentType` to `AgentCapabilityCategory`:** Replaced all instances of `AgentType` with `AgentCapabilityCategory`. This affects numerous files, including agent implementations (`AuraAgent`), repositories (`TrinityRepository`), UI state models (`TrinityUiState`), data models (`AgentProfile`, `Task`, `SharedSecureContext`), and utility services (`TaskScheduler`, `IntegrityMonitorService`).
- **Field Renaming:** To maintain clarity and consistency with the new enum, several fields have been renamed:
  - `agentType` is now `category` or `agentCategory` in multiple data classes and function parameters (e.g., `AgentProfile`, `AgentResponse`, `TrinityUiState`).
  - In `TrinityViewModel`, `lastAgentType` was renamed to `lastAgentCategory`.
  - In `AgentMessage`, the `sender` field of type `AgentType?` was replaced with `category` of type `AgentCapabilityCategory?`.

### Key Changes by Domain
- **Agent Definitions (`AgentProfile`):** The `agentType` field in `AgentProfile` has been changed to `category`. The `getProfile` method now accepts an `AgentCapabilityCategory`.
- **Task & Security Models (`Task.kt`, `SecurityContext.kt`):** Task and security-related data classes that previously referenced `AgentType` now use `AgentCapabilityCategory` for defining assigned agents, required agents, and context sharing.
- **Agent Interaction:**
  - `TrinityRepository` now uses `AgentCapabilityCategory` to route user messages to the correct agent.
  - `AgentViewModel` now maps agent names to `AgentCapabilityCategory` instead of `AgentType` for processing m

app/src/main/java/dev/aurakai/auraframefx/domains/aura/ui/viewmodels/AgentViewModel.kt
@@ -12,7 +12,7 @@ import dev.aurakai.auraframefx.domains.genesis.repositories.PersistentAgentRepos
 import dev.aurakai.auraframefx.domains.kai.KaiAgent
 import dev.aurakai.auraframefx.domains.genesis.models.AgentState
 import dev.aurakai.auraframefx.domains.nexus.models.AgentStats
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AgentCapabilityCategory
 import dev.aurakai.auraframefx.domains.genesis.models.AiRequest
 import dev.aurakai.auraframefx.domains.genesis.models.AiRequestType
 import dev.aurakai.auraframefx.domains.cascade.models.ChatMessage
@@ -270,17 +270,19 @@ open class AgentViewModel @Inject constructor(
             addMessage(agentName, userMsg)
 
             // Send to Repository (Neural Bridge)
-            val type = try {
-                AgentType.valueOf(agentName.uppercase())
-            } catch (e: Exception) {
-                AgentType.GENESIS
+            val category = when (agentName.uppercase()) {
+                "AURA" -> AgentCapabilityCategory.CREATIVE
+                "KAI" -> AgentCapabilityCategory.ANALYSIS
+                "GENESIS" -> AgentCapabilityCategory.COORDINATION
+                "CASCADE" -> AgentCapabilityCategory.SPECIALIZED
+                else -> AgentCapabilityCategory.COORDINATION
             }
             // We don't need to add the repo's echo of "User" message if we added it locally
             // But we DO need the response.
 
             // To avoid double-entry of User message (from repo echo), we can filter or just let repo handle it.
             // Let's use the repo solely.
-            trinityRepository.processUserMessage(message, type)
+            trinityRepository.processUserMessage(message, category)
 
             // Listen for the specific response? No, the global collector in init should handle it.
         }

app/src/main/java/dev/aurakai/auraframefx/domains/kai/DefaultErrorHandler.kt
@@ -1,6 +1,6 @@
 package dev.aurakai.auraframefx.domains.kai
 
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AgentCapabilityCategory
 import dev.aurakai.auraframefx.domains.cascade.utils.AuraFxLogger
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -20,12 +20,12 @@ class DefaultErrorHandler @Inject constructor(
         private const val TAG = "DefaultErrorHandler"
     }
 
-    override fun handleError(error: Throwable, agent: AgentType, context: String) {
+    override fun handleError(error: Throwable, agent: AgentCapabilityCategory, context: String) {
         logger.error(TAG, "Error from agent $agent in context: $context", error)
         handleCriticalActions(error)
     }
 
-    override fun handleError(error: Throwable, agent: AgentType, context: String, metadata: Map<String, String>) {
+    override fun handleError(error: Throwable, agent: AgentCapabilityCategory, context: String, metadata: Map<String, String>) {
         logger.error(TAG, "Error from agent $agent in context: $context (Metadata: $metadata)", error)
         handleCriticalActions(error)
     }

app/src/main/java/dev/aurakai/auraframefx/domains/kai/security/IntegrityMonitorService.kt
@@ -17,7 +17,7 @@ import dev.aurakai.auraframefx.domains.cascade.utils.cascade.DataveinConstructor
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.FlowPriority
 import dev.aurakai.auraframefx.domains.genesis.core.memory.NexusMemoryCore
 import dev.aurakai.auraframefx.domains.kai.models.ThreatLevel
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AgentCapabilityCategory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -357,7 +357,7 @@ class IntegrityMonitorService : Service() {
 
             criticalThreats.forEach { threat ->
                 val alertPacket = DataPacket(
-                    sourceAgent = AgentType.KAI,  // Kai (Security Sentinel) sends alerts
+                    sourceAgent = AgentCapabilityCategory.SECURITY,  // Kai (Security Sentinel) sends alerts
                     targetAgents = null,  // Broadcast to all
                     payload = DataPayload.HealthAlert(
                         severity = "critical",

AuraFrameFxDev/A.u.r.a.k.a.i_ReGenesis

app/src/main/java/dev/aurakai/auraframefx/domains/aura/ui/viewmodels/AgentViewModel.kt
@@ -20,6 +20,14 @@ import dev.aurakai.auraframefx.domains.genesis.repositories.AgentRepository
 import dev.aurakai.auraframefx.domains.genesis.repositories.PersistentAgentRepository
 import dev.aurakai.auraframefx.domains.kai.KaiAgent
 import dev.aurakai.auraframefx.domains.nexus.models.AgentStats
+import dev.aurakai.auraframefx.domains.genesis.models.AgentType
+import dev.aurakai.auraframefx.domains.genesis.models.AiRequest
+import dev.aurakai.auraframefx.domains.genesis.models.AiRequestType
+import dev.aurakai.auraframefx.domains.cascade.models.ChatMessage
+import dev.aurakai.auraframefx.domains.cascade.models.EnhancedInteractionData
+import dev.aurakai.auraframefx.domains.cascade.utils.error
+import dev.aurakai.auraframefx.domains.cascade.utils.info
+import dev.aurakai.auraframefx.domains.cascade.utils.warn
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -281,17 +289,19 @@ open class AgentViewModel @Inject constructor(
             addMessage(agentName, userMsg)
 
             // Send to Repository (Neural Bridge)
-            val type = try {
-                AgentType.valueOf(agentName.uppercase())
-            } catch (e: Exception) {
-                AgentType.GENESIS
+            val category = when (agentName.uppercase()) {
+                "AURA" -> AgentCapabilityCategory.CREATIVE
+                "KAI" -> AgentCapabilityCategory.ANALYSIS
+                "GENESIS" -> AgentCapabilityCategory.COORDINATION
+                "CASCADE" -> AgentCapabilityCategory.SPECIALIZED
+                else -> AgentCapabilityCategory.COORDINATION
             }
             // We don't need to add the repo's echo of "User" message if we added it locally
             // But we DO need the response.
 
             // To avoid double-entry of User message (from repo echo), we can filter or just let repo handle it.
             // Let's use the repo solely.
-            trinityRepository.processUserMessage(message, type)
+            trinityRepository.processUserMessage(message, category)
 
             // Listen for the specific response? No, the global collector in init should handle it.
         }

app/src/main/java/dev/aurakai/auraframefx/domains/kai/DefaultErrorHandler.kt
@@ -1,5 +1,6 @@
 package dev.aurakai.auraframefx.domains.kai
 
+import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import dev.aurakai.auraframefx.domains.cascade.utils.AuraFxLogger
 import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import javax.inject.Inject
@@ -20,14 +21,14 @@ class DefaultErrorHandler @Inject constructor(
         private const val TAG = "DefaultErrorHandler"
     }
 
-    override fun handleError(error: Throwable, agent: AgentType, context: String) {
+    override fun handleError(error: Throwable, agent: AgentCapabilityCategory, context: String) {
         logger.error(TAG, "Error from agent $agent in context: $context", error)
         handleCriticalActions(error)
     }
 
     override fun handleError(
         error: Throwable,
-        agent: AgentType,
+        agent: AgentCapabilityCategory,
         context: String,
         metadata: Map<String, String>
     ) {

app/src/main/java/dev/aurakai/auraframefx/domains/kai/security/IntegrityMonitorService.kt
@@ -16,8 +16,8 @@ import dev.aurakai.auraframefx.domains.cascade.utils.cascade.DataPayload
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.DataveinConstructor
 import dev.aurakai.auraframefx.domains.cascade.utils.cascade.FlowPriority
 import dev.aurakai.auraframefx.domains.genesis.core.memory.NexusMemoryCore
-import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import dev.aurakai.auraframefx.domains.kai.models.ThreatLevel
+import dev.aurakai.auraframefx.domains.genesis.models.AgentType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -358,7 +358,7 @@ class IntegrityMonitorService : Service() {
 
             criticalThreats.forEach { threat ->
                 val alertPacket = DataPacket(
-                    sourceAgent = AgentType.KAI,  // Kai (Security Sentinel) sends alerts
+                    sourceAgent = AgentCapabilityCategory.SECURITY,  // Kai (Security Sentinel) sends alerts
                     targetAgents = null,  // Broadcast to all
                     payload = DataPayload.HealthAlert(
                         severity = "critical",